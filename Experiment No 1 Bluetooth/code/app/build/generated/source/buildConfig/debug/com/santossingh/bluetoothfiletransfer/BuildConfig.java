de8e4271dd4bd158610319f0affe14713fe8600a
vrajesh-art <vrajeshshetty2000@gmail.com>
2024-02-14 08:54:17 +0530

    first commit

---
+++ Experiment No 1 Bluetooth/code/app/build/generated/source/buildConfig/debug/com/santossingh/bluetoothfiletransfer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.santossingh.bluetoothfiletransfer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.santossingh.bluetoothfiletransfer";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
